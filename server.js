27a061a365ad59cfa883354538a2cf70da84e0d7
Erikson <chris.s.erickson@gmail.com>
2022-08-18 18:54:47 -0400

    mysql start via server logic added

--- server.js
+++ server.js
@@ -4,10 +4,25 @@ const express = require ('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// mysql import
+const mysql= require ('mysql2');
+
 // express middleware
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+// connect app to MySQL
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 // test express
 app.get('/', (req, res) => {
